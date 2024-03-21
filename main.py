76ffedf6e53c7f57bd6f2c8f86da97ae7aa48445
Finn Connell <finn.bc@protonmail.com>
2024-03-21 13:06:43 +1300

    first successful scrape

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-#!/usr/bin/env python
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+response = requests.get("https://bulbapedia.bulbagarden.net/wiki/Wartortle_(Pok%C3%A9mon)")
+page = BeautifulSoup(response.content,"html.parser")
+title = page.find("title")
+print(title)
