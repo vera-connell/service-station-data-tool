6575d1069cdb02ad6a6570408f8c39397cd86a07
Finn Connell <finn.bc@protonmail.com>
2024-03-21 13:19:29 +1300

    scrapes all plain text. needs refactor

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ response = requests.get("https://bulbapedia.bulbagarden.net/wiki/Wartortle_(Pok%
 page = BeautifulSoup(response.content,"html.parser")
 title = page.find("title")
 print(title)
+
+movesResponse = requests.get("https://bulbapedia.bulbagarden.net/wiki/Wartortle_(Pok%C3%A9mon)#Learnset")
+movesInfo = BeautifulSoup(response.content,"html.parser")
+learnset = movesInfo.find_all(text=True)
+print(learnset)
